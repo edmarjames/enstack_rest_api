3573cb03c8dc8fecf1edb6b7c7bf9ffe9ce57b77
edmar.james <edmarjamesbautista@gmail.com>
2025-02-27 15:47:33 +0800

    Update database.db

--- api.py
+++ api.py
@@ -12,9 +12,9 @@ from utils import letter_args, letterFields, login_args
 
 load_dotenv()
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 api = Api(app)
